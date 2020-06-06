b3b0604f36b6f41c0b45d4eb1da2bc4357411175
springleeo <springleeo@163.com>
2020-06-06 15:26:15 +0800

    django model 增删改查数据库 本次内容截止教程3-4

--- apps/message/models.py
+++ apps/message/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class UserMessage(models.Model):
+    name = models.CharField("用户名", max_length=20)
+    email = models.EmailField("邮箱")
+    address = models.CharField("地址", max_length=100)
+    message = models.CharField("留言", max_length=200)
+
+    class Meta:
+        verbose_name = "用户留言信息"
+        verbose_name_plural = verbose_name
+
--- apps/message/views.py
+++ apps/message/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
 
+from .models import UserMessage
+
 
 def get_form(request):
+    # 查询数据库数据
+    # all_message = UserMessage.objects.all()
+
+    # all_message = UserMessage.objects.filter(name='和平')
+    # for message in all_message:
+    #     print(message.name)
+    #     print()
+
+    # 保存数据到数据库
+    # message = UserMessage()
+    # message.name = '夏天'
+    # message.email = '1@4.com'
+    # message.address = '福州'
+    # message.save()
+
+    # 表单添加数据
+    # if request.method == 'POST':
+    #     message = UserMessage()
+    #     message.name = request.POST.get('name', '')
+    #     message.email = request.POST.get('email', '')
+    #     message.address = request.POST.get('address', '')
+    #     message.message = request.POST.get('message', '')
+    #     message.save()
+
+    # 表单删除数据
+    if request.method == 'POST':
+        message = UserMessage.objects.filter(name=request.POST.get('name'))
+        message.delete()
+
+
+    print()
+
     return render(request, 'message_form.html')
